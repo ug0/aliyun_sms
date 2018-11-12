68540a779115e26fb9dd205c7a61f71618893304
ug0 <ug600@icloud.com>
2018-11-12 16:25:39 +0800

    First commit

---
+++ lib/aliyun/sms.ex
@@ -0,0 +1,21 @@
+defmodule Aliyun.Sms do
+  @moduledoc """
+  Documentation for Aliyun.Sms.
+  """
+
+  @doc """
+  发送短信.
+
+  ## Examples
+
+      iex> Aliyun.Sms.send()
+      {:ok, %{...}}
+
+      iex> Aliyun.Sms.send()
+      {:error, %{...}}
+
+  """
+  def send() do
+  end
+
+end
---
+++ test/aliyun/sms_test.exs
@@ -0,0 +1,4 @@
+defmodule Aliyun.SmsTest do
+  use ExUnit.Case
+  doctest Aliyun.Sms
+end
