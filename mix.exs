4954c24d794131a8f94c91aab0983f61f29f1ac8
ug0 <ug600@icloud.com>
2018-11-13 13:56:26 +0800

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -26,6 +26,7 @@ defmodule AliyunSms.MixProject do
       {:jason, "~> 1.1"},
       {:aliyun_util, "~> 0.2.0"},
       {:httpoison, "~> 1.4"},
+      {:ex_doc, "~> 0.18", only: :dev},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
