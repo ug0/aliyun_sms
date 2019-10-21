524e5728e3ec863048981ceface8aea2c628e3ee
ug0 <ug600@icloud.com>
2019-10-21 14:07:40 +0800

    Use confex to resolve aliyun secret config

--- mix.exs
+++ mix.exs
@@ -27,6 +27,7 @@ defmodule AliyunSms.MixProject do
       {:jason, "~> 1.1"},
       {:aliyun_util, "~> 0.3.3"},
       {:httpoison, "~> 1.4"},
+      {:confex, "~> 3.4"},
       {:ex_doc, "~> 0.20", only: :dev},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
