91b349337ab6d093be77d362a42e8113c4afe245
ug0 <18638328+ug0@users.noreply.github.com>
2025-05-12 09:46:05 +0800

    feat: allow to pass encoded json string as `template_params`

--- lib/aliyun_sms.ex
+++ lib/aliyun_sms.ex
@@ -4,6 +4,9 @@ defmodule Aliyun.Sms do
 
   ## Examples
 
+      iex> Aliyun.Sms.Client.send_sms("1500000000", "阿里云短信测试专用", "SMS_0000", ~s[{"code":"222333"}])
+      {:ok, %{"Code" => "OK", "Message" => "OK", "BizId" => "700000000000000000^0", "RequestId" => "A0000000-3CC1-4000-8000-E00000000000"}}
+
       iex> Aliyun.Sms.Client.send_sms("1500000000", "阿里云短信测试专用", "SMS_0000", %{code: "222333"})
       {:ok, %{"Code" => "OK", "Message" => "OK", "BizId" => "700000000000000000^0", "RequestId" => "A0000000-3CC1-4000-8000-E00000000000"}}
 
@@ -14,7 +17,7 @@ defmodule Aliyun.Sms do
       {:error, %Req.TransportError{reason: :ehostunreach}}
 
   """
-  @spec send_sms(String.t(), String.t(), String.t(), map(), String.t() | nil, String.t() | nil) ::
+  @spec send_sms(String.t(), String.t(), String.t(), map() | String.t(), String.t() | nil, String.t() | nil) ::
           {:ok, map()} | {:error, map()}
 
   defdelegate send_sms(phones, sign_name, template, template_params, up_extend_code \\ nil, out_id \\ nil), to: Aliyun.Sms.Client
--- lib/aliyun_sms/client.ex
+++ lib/aliyun_sms/client.ex
@@ -4,6 +4,9 @@ defmodule Aliyun.Sms.Client do
 
   ## Examples
 
+      iex> Aliyun.Sms.Client.send_sms("1500000000", "阿里云短信测试专用", "SMS_0000", ~s[{"code":"222333"}])
+      {:ok, %{"Code" => "OK", "Message" => "OK", "BizId" => "700000000000000000^0", "RequestId" => "A0000000-3CC1-4000-8000-E00000000000"}}
+
       iex> Aliyun.Sms.Client.send_sms("1500000000", "阿里云短信测试专用", "SMS_0000", %{code: "222333"})
       {:ok, %{"Code" => "OK", "Message" => "OK", "BizId" => "700000000000000000^0", "RequestId" => "A0000000-3CC1-4000-8000-E00000000000"}}
 
@@ -13,17 +16,29 @@ defmodule Aliyun.Sms.Client do
       iex> Aliyun.Sms.Client.send_sms("1500000000", "阿里云短信测试专用", "SMS_0000", %{code: "222333"})
       {:error, %Req.TransportError{reason: :ehostunreach}}
   """
-  def send_sms(phones, sign_name, template, template_params = %{}, up_extend_code \\ nil, out_id \\ nil) do
-    request(
-      %{
-        "PhoneNumbers" => phones,
-        "SignName" => sign_name,
-        "TemplateCode" => template,
-        "TemplateParam" => JSON.encode!(template_params),
-        "SmsUpExtendCode" => up_extend_code,
-        "OutId" => out_id
-      }
-    )
+  @spec send_sms(
+          String.t(),
+          String.t(),
+          String.t(),
+          map() | String.t(),
+          String.t() | nil,
+          String.t() | nil
+        ) :: {:ok, map()} | {:error, map()}
+  def send_sms(phones, sign_name, template, template_params, up_extend_code \\ nil, out_id \\ nil)
+
+  def send_sms(phones, sign_name, template, template_params = %{}, up_extend_code, out_id) do
+    send_sms(phones, sign_name, template, JSON.encode!(template_params), up_extend_code, out_id)
+  end
+
+  def send_sms(phones, sign_name, template, template_params, up_extend_code, out_id) do
+    request(%{
+      "PhoneNumbers" => phones,
+      "SignName" => sign_name,
+      "TemplateCode" => template,
+      "TemplateParam" => template_params,
+      "SmsUpExtendCode" => up_extend_code,
+      "OutId" => out_id
+    })
   end
 
   @headers %{
@@ -32,11 +47,18 @@ defmodule Aliyun.Sms.Client do
   }
   @url "https://dysmsapi.aliyuncs.com/"
   defp request(query_params) do
-    config = Aliyun.Util.Config.new!(%{access_key_id: access_key_id(), access_key_secret: access_key_secret()})
-    headers = Map.merge(@headers, %{
-      "x-acs-date" => Aliyun.Util.Time.gmt_now_iso8601(),
-      "x-acs-signature-nonce" => :crypto.strong_rand_bytes(16) |> Base.encode16() |> String.downcase()
-    })
+    config =
+      Aliyun.Util.Config.new!(%{
+        access_key_id: access_key_id(),
+        access_key_secret: access_key_secret()
+      })
+
+    headers =
+      Map.merge(@headers, %{
+        "x-acs-date" => Aliyun.Util.Time.gmt_now_iso8601(),
+        "x-acs-signature-nonce" =>
+          :crypto.strong_rand_bytes(16) |> Base.encode16() |> String.downcase()
+      })
 
     config
     |> Aliyun.Util.Request.build!(:post, @url, query_params, headers, "")
